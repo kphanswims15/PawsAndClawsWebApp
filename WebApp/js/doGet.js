c9087a777c68371cd3aaabec1e2fe52d6e9477dc
Kimi <phanlkim@comcast.net>
2019-03-27 22:48:49 -0700

    Made more headway on the app

--- WebApp/js/doGet.js
+++ WebApp/js/doGet.js
@@ -7,36 +7,47 @@
 //*********************************************************************************
 
 function doGet(e) {
-    var template, tName;
-    tName = "Idaho Speedgolf Championship";
-    if (e.parameter.player) { //player scoring
-      template = HtmlService.createTemplateFromFile('html/PlayerScore-Starter');
-      template.data = {playerId: e.parameter.player};
-      return template
-      .evaluate()
+  var template, faviconUrl, tName;
+  faviconUrl = "https://zxqbug.bn.files.1drv.com/y4mHkrhV62YcBzQfBS3Tso5iSTELyIYicuJAp8_x6M-BJfXhrpbrLGXorCS1asKFcLdZG0xcRMjLnJuzcRJoQUIegklSal8uOkMuMGkjHSytYI5xkjPmHpNQwlfoS4_rhMriF_Ow1ucxopjA2oLNyBhUAV4XGuPRj0CF7c-KWVblh9qtakZH6-ewknl4BtzR5Uvxv2zBXy6SMUl22pANI7hHA?width=500&height=500&cropmode=none"
+  tName = "Idaho Speedgolf Championship";
+  if (e.parameter.player) { //player scoring
+    template = HtmlService.createTemplateFromFile('html/PlayerScore-Starter');
+    template.data = {player: e.parameter.player, playerId: e.parameter.playerId};
+    return template
+    .evaluate()
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setTitle("Score for " + e.parameter.player)
+    .setFaviconUrl(faviconUrl);
+  } if (e.parameter.page == "Welcome") { //show leaderboard under constructino page
+    return HtmlService.createHtmlOutputFromFile('html/Welcome')
       .setSandboxMode(HtmlService.SandboxMode.IFRAME)
       .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-      .setTitle("Score for " + e.parameter.player)
-    } if (e.parameter.page) { //show leaderboard under constructino page
-      return HtmlService.createHtmlOutputFromFile('html/LeaderboardUnderConstruction')
-        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-        .setTitle("SpeedScore LIVE Leaderboard")
-    } else { //login page
-      template = HtmlService.createTemplateFromFile('html/LoginUI');
-      return template
-      .evaluate()
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-      .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-      .setTitle("Paws and Claws")
-    }
+      .setTitle("Welcome");
+  } else if (e.parameter.page == "Door") {
+    return HtmlService.createHtmlOutputFromFile('html/Door')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setTitle("Door");
+  } else if (e.parameter.page == "Applicator") {
+    return HtmlService.createHtmlOutputFromFile('html/Applicator')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setTitle("Applicator");
+  } else { //login page
+    template = HtmlService.createTemplateFromFile('html/LoginUI');
+    return template
+    .evaluate()
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setTitle("Paws and Claws");
   }
-    
-   //include: Allows us to include files using templated HTML, per Google's best practices 
-  //(https://developers.google.com/apps-script/guides/html/best-practices)
-  function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename)
-        .getContent();
-  }     
+}
   
+ //include: Allows us to include files using templated HTML, per Google's best practices 
+//(https://developers.google.com/apps-script/guides/html/best-practices)
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+      .getContent();
+}  
   
